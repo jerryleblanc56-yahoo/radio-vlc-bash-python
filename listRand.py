bfc774ba2fde41ca47b367574e4dcdfdc4ce4038
jerryleblanc56 <jerryleblanc56@yahoo.com>
2014-09-28 13:47:03 -0700

    Create listRand.py
    related to jukebox
    # get a random file from structure 
    # http://stackoverflow.com/questions/6411811/randomly-selecting-a-file-from-a-tree-of-directories-in-a-completely-fair-manner

---
+++ listRand.py
@@ -0,0 +1,11 @@
+# get a random file from structure 
+# http://stackoverflow.com/questions/6411811/randomly-selecting-a-file-from-a-tree-of-directories-in-a-completely-fair-manner 
+#
+import os
+import random
+files = [os.path.join(path, filename)
+         for path, dirs, files in os.walk("/mnt/usb1/music/iTunes")
+         for filename in files
+         if filename.endswith(".m4a")]
+#print( random.choice(files) )
+print( sorted(files, key=lambda k: random.random()) )
